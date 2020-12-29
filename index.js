970fb56b8078a397868abffee24324f59ca75f56
Milo Bravman <Milobravman20@gmail.com>
2020-12-29 13:42:10 -0500

    index.js setup

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+/*
+This repo is ment to practice Javascript capablities that I learn from either textbook
+*/
+
 let today = new Date;
 
 console.log(today.getDay());
-
+console.log(today.toString());
 
