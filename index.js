af8992fb9912ee269d375aff13f6fa0a0bf45c50
Milo Bravman <Milobravman20@gmail.com>
2020-12-29 14:13:58 -0500

    I don't know how to import modules

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 This repo is ment to practice Javascript capablities that I learn from either textbook
 */
 
+//import {exporting} from "./exporting.js"
+
 let today = new Date;
 
 console.log(today.getDay());
 console.log(today.toString());
+//exporting()
 
