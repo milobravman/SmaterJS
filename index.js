293d189b1b142202529edf1c5c5844b94c9764c5
Milo Bravman <Milobravman20@gmail.com>
2020-12-30 17:25:42 -0500

    importing class

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ This repo is ment to practice Javascript capablities that I learn from either te
 //import {exporting} from "./exporting.js"
 import k from "./modules/exporting.js"
 
-// import User from "./modules"
+import User, {printAge, printName} from "./modules/user.js"
 
 let today = new Date;
 
 console.log(today.getDay());
 console.log(today.toString());
 console.log(k)
+
+let user = new User("bob", 10)
+console.log(user)
+printAge(user)
 //exporting()
 
