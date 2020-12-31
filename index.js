4a3d1a76ed548984277306bb13ef828c63ebd9e1
Milo Bravman <Milobravman20@gmail.com>
2020-12-31 01:12:01 -0500

    animiation nation

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ This repo is ment to practice Javascript capablities that I learn from either te
 
 //import {exporting} from "./exporting.js"
 import k from "./modules/exporting.js"
-
 import User, {printAge, printName} from "./modules/user.js"
 
-let today = new Date;
 
+let today = new Date;
 console.log(today.getDay());
 console.log(today.toString());
 console.log(k)
@@ -17,8 +16,14 @@ let user = new User("bob", 10)
 console.log(user)
 printAge(user)
 
-let btn = document.querySelector("#Youtube")
+let Youbtn = document.querySelector("#Youtube")
+Youbtn.addEventListener("click" , (()=> location = "https://www.youtube.com/"))
 
-btn.addEventListener("click" , (()=> location = "https://www.youtube.com/"))
+document.querySelector("#animation-btn").addEventListener("click",(()=> {
+    let animatedDiv = document.querySelector("#toAnimation")
+    animatedDiv.className = "animation"
+}) )
+
+
 //exporting()
 
