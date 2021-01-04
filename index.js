affbc1bd85c80457dccf484f285298a9a786d788
Milo Bravman <Milobravman20@gmail.com>
2021-01-04 17:23:13 -0500

    Ajax api call not allowed

--- index.js
+++ index.js
@@ -29,9 +29,25 @@ document.querySelector("#animation-rest").addEventListener("click", (()=>{
     resetDiv.className = ""
 }))
 
+document.querySelector("#using-Ajax").addEventListener("click", loadDoc)
+
 localStorage.setItem("name","John Doe");
 console.log(localStorage.getItem("name"))
 
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("demo").innerHTML = this.responseText;
+      }
+      else {
+          document.getElementById("demo").innerHTML = this.statusText
+      }
+    };
+    xhttp.open("GET", "https://www.w3schools.com/js/ajax_info.txt", true);
+    xhttp.send();
+  }
 
 //exporting()
 
